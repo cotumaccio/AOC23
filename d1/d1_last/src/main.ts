9d415df548ad10419593b46258cd596a1bebb315
Gianfranco Cotumaccio <cotumaccio@gmail.com>
2023-12-07 17:53:03 +0100

    Fix

--- d1/d1_last/src/main.ts
+++ d1/d1_last/src/main.ts
@@ -1,7 +1,58 @@
-// import './style.css';
-import data from './input.json';
+import data from './data/input.json';
 
 console.time("Solution");
+
+const convertToNumber = (literal: string): string => {
+  let number: string = "";
+  switch (literal) {
+    case "one":
+      number = "1";
+      break;
+    case "two":
+      number = "2";
+      break;
+    case "three":
+      number = "3";
+      break;
+    case "four":
+      number = "4";
+      break;
+    case "five":
+      number = "5";
+      break;
+    case "six":
+      number = "6";
+      break;
+    case "seven":
+      number = "7";
+      break;
+    case "eight":
+      number = "8";
+      break;
+    case "nine":
+      number = "9";
+      break;
+  }
+  return number;
+}
+
+const checkString = (row: string): void => {
+  console.log("Input:", row);
+  const capturingRegex = /(?<literals>one|two|three|four|five|six|seven|eight|nine)|\*/gm;
+  const found= row.match(capturingRegex);
+  console.log("Founded:", found);
+
+  let first: string = found[0];
+  let last: string = found[found.length - 1];
+
+  if ( row.startsWith(first) && row.endsWith(last) ) {
+    // console.log("True");
+    let computed: string = convertToNumber(first) + convertToNumber(last);
+    console.log(computed);
+  }
+}
+
+checkString(data.data[9]);
 
 const extractNumbers = (row: string): number => {
   let matches = row.match(/\d+/g);
